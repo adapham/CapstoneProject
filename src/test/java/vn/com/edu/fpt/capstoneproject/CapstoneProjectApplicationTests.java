5369323e43a190237313115ec1187eaa0277eedd
Snail <Phamada2803@gmail.com>
2023-04-06 16:32:18 +0700

    init project

---
+++ src/test/java/vn/com/edu/fpt/capstoneproject/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.edu.fpt.capstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
